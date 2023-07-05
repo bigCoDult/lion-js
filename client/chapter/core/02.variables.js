e71029ddaa33fc8c0474197142ebed0543491a4a
bigCoDult <qqqqmmss11@gmail.com>
2023-07-05 09:22:04 +0900

    backup

--- client/chapter/core/02.variables.js
+++ client/chapter/core/02.variables.js
@@ -2,34 +2,6 @@
 /* Variables          */
 /* ------------------ */
 
-<<<<<<< HEAD
-let name = 'John';
-let admin = name;
-
-let planet__current = 'earth';
-let userName__current = 'bill evans';
-
-// console.log(admin);
-
-/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
-
-// - 갯수 별 상품 가격 계산하기
-let price, number_of_stuff, discountPct;
-// - 구매 제품 가격의 총 합
-let price__total;
-// - 1년 기준 일(day)자 수
-let days_in_year;
-// - 구매 결제 여부
-let flagPurchased;
-// - 구매 결제 내역
-let logPurchased;
-// - 브랜드 접두사
-const BRAND_PREFIX = '짱 멋진';
-// - 오늘의 운세
-let luckToday;
-// - 상품 정보
-const DATA__STUFF = '좋은거';
-=======
 
 let admin; // undefined
 let name; // undefined
@@ -77,16 +49,11 @@ let fortuneOfToday;
 
 // - 상품 정보
 const productInfomation = '';
->>>>>>> 5872b8a27f3ff40b196dd2e0b3882c8db8edc9f0
 
 
 /* variables ----------------------------------------------------------- */
 
 
-<<<<<<< HEAD
-/* constant variables -------------------------------------------------- */
-
-=======
 
 
 
@@ -127,4 +94,3 @@ const REJECTED = 'api/rejected';
 
 
 
->>>>>>> 5872b8a27f3ff40b196dd2e0b3882c8db8edc9f0
--- client/main.js
+++ client/main.js
@@ -1,7 +1,2 @@
 // console.log('hello js!');
-<<<<<<< HEAD
-
-
-=======
 // confirm('정말 지울꺼야..?');
->>>>>>> 5872b8a27f3ff40b196dd2e0b3882c8db8edc9f0
