75ecaae6321513b5753b251ce47a93c9289bf8d6
bigCoDult <qqqqmmss11@gmail.com>
2023-07-18 10:07:31 +0900

    backup

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,3 +27,34 @@ module.exports = {
         
     }
 }
+
+
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: 'eslint:recommended',
+    globals:{
+      getNode:'readOnly',
+    },
+    overrides: [
+      {
+        env: {
+          node: true,
+        },
+        files: ['.eslintrc.{js,cjs}'],
+        parserOptions: {
+          sourceType: 'script',
+        },
+      },
+    ],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  };
