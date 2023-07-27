bb624f4fba3c52f2962c90f2d1b5e6d742c43ab0
bigCoDult <qqqqmmss11@gmail.com>
2023-07-27 15:44:49 +0900

    로컬 스토리지 강의 끝

--- client/main.js
+++ client/main.js
@@ -1,3 +1,17 @@
-console.log('hello js!');
+import { getNode, setStorage, getStorage } from './lib/index.js';
 
+const textField = getNode('#textField');
 
+function handleTextField() {
+  const value = this.value;
+  setStorage('text', value);
+  console.log(value);
+}
+function init() {
+  getStorage('text').then((res) => {
+    textField.value = res;
+  });
+}
+
+textField.addEventListener('input', handleTextField);
+globalThis.addEventListener('DOMContentLoaded', init);
